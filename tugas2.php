bb66d7db0f266fcab4f92d863b756aba318865d0
Dwinasrtk <87896781+Dwinasrtk@users.noreply.github.com>
2021-08-08 15:45:53 +0700

    Add files via upload

---
+++ tugas2.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pertemuan 3 tugas 2 Dwina Sartika</title>
+</head>
+<body bgcolor="cornsilk">
+    <h1 align="center">Pertemuan ke 3</h1>
+    <h2 align="center">Tugas 2</h2>
+    <p align="center">Dwina Sartika</p>
+    <center>Pemrograman Web dan Perangkat Bergerak</center>
+    <a href="https://www.instagram.com/">Instagram</a>
+    <hr>
+    <table cellspacing="0" cellpadding="3" border="1" width="200px" height="100px" align="center">
+        <tr>
+            <th>ini adalah tabel</th>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td rowspan="2"></td>
+        </tr>
+        <tr>
+            <td colspan="2"></td>
+        </tr>
+    </table>
+    <pre>Berikut adalah daftar gambar</pre>
+    <ol>
+        <li>Gambar Panda</li>
+    </ol>
+    <ul>
+        <li>Deskripsi tentang panda</li>
+    </ul>
+    <dl>
+        <dt>PANDA</dt>
+        <dd>Panda adalah hewan mamalia yang diklasifikasikan ke dalam keluarga beruang. Panda memiliki tubuh yang berwarna hitam dan putih. Panda biasa memakan bambu.</dd>
+    </dl>
+    <b>Foto Panda dari Google</b>
+    <img src="https://asset.kompas.com/crops/K6s8BiFV2Y3tzduBV4Fuc00J5KU=/0x0:1000x667/750x500/data/photo/2021/07/11/60ea4847a2383.jpg">
+    <font face="Georgia" size="16"></font>
+    <div style="width:200px;height=300px;"></div>
+    <hr noshade>
+    <p align="center">by:Dwina Sartika</p>
+</body>
+</html>
